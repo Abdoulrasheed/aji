f059b7e3093798d04992a9373956f6317e331bef
abdoulrasheed <abdulrasheedibrahim47@gmail.com>
2019-09-01 21:55:08 +0100

    Update settings

--- modern_market/settings.py
+++ modern_market/settings.py
@@ -26,7 +26,7 @@ with open(BASE_DIR + '/key/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.herokuapp.com']
 
 
 # Application definition
