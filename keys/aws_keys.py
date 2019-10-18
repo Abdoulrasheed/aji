ccf441fc3e7e261479bf53466c751198b4f024ea
Abdulrasheed <abdulrasheedibrahim@mautech.edu.ng>
2019-10-18 22:42:44 +0100

    Update keys

--- keys/aws_keys.py
+++ keys/aws_keys.py
@@ -1,3 +1,3 @@
-APPSYNC_API_KEY = 'da2-6hwfwofycjftvbi6odi3ga6h2i'
-APPSYNC_API_ENDPOINT_URL = 'https://vgyukhl23vexrgj4kws4gvq5pu.appsync-api.us-east-2.amazonaws.com/graphql'
+APPSYNC_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxx'
+APPSYNC_API_ENDPOINT_URL = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
